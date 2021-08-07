ff4ea201a0caee0906538803e6416c7b1112df98
lgdxiaozhu1987 <lgdxiaozhu1987@outlook.com>
2021-08-07 10:03:07 +0800

    精简后的1.0版本

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,4 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:git@github.com:lgdxiaozhu1987/lgdxiaozhu1987.github.io.git main
+git push -f git@github.com:lgdxiaozhu1987/lgdxiaozhu1987.github.io.git master
